ea91022a7b9fc2eea6909ed359170600d4ee0d91
C461 <kozefra@gmai.com>
2024-02-04 20:46:38 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
-import app from "./app.js";
-import { connectDB } from "./db.js";
+
 
 const port = process.env.PORT || 4000;
 try {
