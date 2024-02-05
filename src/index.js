2d78732541ee74d7099cd9c99a002c54ba8e89ab
C461 <kozefra@gmai.com>
2024-02-04 20:44:40 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
+import app from "./app.js";
+import { connectDB } from "./db.js";
 
 const port = process.env.PORT || 4000;
-connectDB();
-app.listen(port, '0.0.0.0');
-console.log("Server on port", port );
+try {
+  connectDB();
+  app.listen(port, '0.0.0.0', () => {
+    console.log("Server on port", port);
+  });
+} catch (error) {
+  console.error("Error starting server:", error);
+}
