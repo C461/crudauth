5b5cbaecf476f2875bf48eb6dd09593fe4cc53fa
C461 <kozefra@gmai.com>
2024-02-04 23:05:02 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
+const cors = require("cors");
+
+const corsOptions = {
+  origin: [
+    "https://crudfront1-production.up.railway.app/register ",
+    "https://crudfront1-production.up.railway.app/login",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 4000;
 try {
   connectDB();
-  app.listen(port, '0.0.0.0', () => {
+  app.listen(port, "0.0.0.0", () => {
     console.log("Server on port", port);
   });
 } catch (error) {
