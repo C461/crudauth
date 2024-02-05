665231fc506bf98ebdf404bfb0502fd0daea4b9b
C461 <kozefra@gmai.com>
2024-02-05 16:36:34 -0500

    updating

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
-import authRoutes from "./Routes/auth.routes.js";
-import taskRoutes from "./Routes/tasks.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import authRoutes from "./Routes/auth.routes.js";
+import taskRoutes from "./Routes/tasks.routes.js";
+
 
 const app = express();
 
@@ -19,5 +21,15 @@ app.use(cookieParser());
 
 app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const path = await import("path");
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) => {
+    console.log(path.resolve("client", "dist", "index.html") );
+    res.sendFile(path.resolve("client", "dist", "index.html"));
+  });
+}
 
 export default app;
